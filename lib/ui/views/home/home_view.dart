4296528ee3cb9d1354d94941ee4e866163394060
iwind <134185860+iwindd@users.noreply.github.com>
2024-04-26 17:26:59 +0700

    lint

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -1,12 +1,11 @@
 import 'package:boltclone_stacked/ui/widgets/MapSearchBottomSheet.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:stacked/stacked.dart';
 import 'home_viewmodel.dart';
 
 class HomeView extends StackedView<HomeViewModel> {
-  const HomeView({Key? key}) : super(key: key);
+  const HomeView({super.key});
 
   @override
   Widget builder(
