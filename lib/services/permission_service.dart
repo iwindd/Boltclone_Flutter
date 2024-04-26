3a62da60408e18f117518062e32d5854541d6034
iwind <134185860+iwindd@users.noreply.github.com>
2024-04-26 12:39:49 +0700

    + permission guard

--- lib/services/permission_service.dart
+++ lib/services/permission_service.dart
@@ -1,1 +1,52 @@
-class PermissionService {}
+import 'package:location/location.dart';
+
+class PermissionService {
+  Location location = Location();
+
+  bool isLocationPerGiven = false;
+  bool isLocationSerGiven = false;
+
+  PermissionService() {
+    location.changeSettings(accuracy: LocationAccuracy.low);
+    location.hasPermission().then((isGiven) {
+      if (isGiven == PermissionStatus.granted ||
+          isGiven == PermissionStatus.grantedLimited) {
+        location.serviceEnabled().then((isEnabled) {
+          if (isEnabled) {
+            isLocationSerGiven = true;
+          } else {
+            isLocationSerGiven = false;
+          }
+        });
+
+        isLocationPerGiven = true;
+      } else {
+        isLocationPerGiven = false;
+      }
+    });
+  }
+
+  requestAppLocationPermission() {
+    location.requestPermission().then((isGiven) {
+      if (isGiven == PermissionStatus.granted ||
+          isGiven == PermissionStatus.grantedLimited) {
+        isLocationPerGiven = true;
+      } else {
+        isLocationPerGiven = false;
+      }
+    });
+  }
+
+  requestLocationServiceToEnable() {
+    location.requestService().then((isGiven) {
+      isLocationSerGiven = isGiven;
+    });
+  }
+
+  Future<bool> checkAppLocationGranted() async {
+    PermissionStatus permission = await location.hasPermission();
+
+    return permission == PermissionStatus.granted ||
+        permission == PermissionStatus.grantedLimited;
+  }
+}
--- lib/ui/views/startup/startup_viewmodel.dart
+++ lib/ui/views/startup/startup_viewmodel.dart
@@ -1,9 +1,11 @@
+import 'package:boltclone_stacked/services/permission_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:boltclone_stacked/app/app.locator.dart';
 import 'package:boltclone_stacked/app/app.router.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class StartupViewModel extends BaseViewModel {
+  final _permissionService = locator<PermissionService>();
   final _navigationService = locator<NavigationService>();
 
   // Place anything here that needs to happen before we get into the application
@@ -12,6 +14,11 @@ class StartupViewModel extends BaseViewModel {
 
     // This is where you can make decisions on where your app should navigate when
     // you have custom startup logic
+
+    if (_permissionService.isLocationPerGiven == false ||
+        _permissionService.isLocationSerGiven == false) {
+      _navigationService.replaceWith(Routes.permissionView);
+    }
 
     _navigationService.replaceWithHomeView();
   }
