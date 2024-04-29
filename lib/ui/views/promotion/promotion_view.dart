c5341fb87daaa69f828699e6f8d9b42e96a8c1d2
iwind <134185860+iwindd@users.noreply.github.com>
2024-04-29 17:40:53 +0700

    promotion

--- lib/ui/views/promotion/promotion_view.dart
+++ lib/ui/views/promotion/promotion_view.dart
@@ -12,14 +12,54 @@ class PromotionView extends StackedView<PromotionViewModel> {
     PromotionViewModel viewModel,
     Widget? child,
   ) {
+    final theme = Theme.of(context);
+
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
+      backgroundColor: theme.colorScheme.secondary,
       appBar: AppBar(
         leading: IconButton(
             onPressed: viewModel.onClose, icon: const Icon(Icons.arrow_back)),
       ),
-      body: Container(
-        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            decoration: BoxDecoration(
+              color: theme.colorScheme.surface,
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(10),
+                bottomRight: Radius.circular(10),
+              ),
+            ),
+            width: MediaQuery.of(context).size.width,
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "โปรโมชัน",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 23,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                SizedBox(height: 10),
+                ListTile(
+                  title: Text("กรอกรหัสโปรโมชัน"),
+                  leading: Icon(Icons.sell_outlined),
+                  trailing: Icon(
+                    Icons.arrow_forward_ios,
+                    size: 12,
+                    color: Colors.black38,
+                  ),
+                  shape: Border(
+                    bottom: BorderSide(color: Colors.black12, width: 1.0),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
