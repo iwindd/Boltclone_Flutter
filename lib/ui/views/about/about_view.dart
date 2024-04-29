a5b91ac85d2d3ada2d567cb83bbd18e32df5ed81
iwind <134185860+iwindd@users.noreply.github.com>
2024-04-29 16:00:28 +0700

    about

--- lib/ui/views/about/about_view.dart
+++ lib/ui/views/about/about_view.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
-
 import 'about_viewmodel.dart';
 
 class AboutView extends StackedView<AboutViewModel> {
+  // ignore: use_super_parameters
   const AboutView({Key? key}) : super(key: key);
 
   @override
@@ -12,14 +12,78 @@ class AboutView extends StackedView<AboutViewModel> {
     AboutViewModel viewModel,
     Widget? child,
   ) {
+    List<MenuItem> menuLists = [
+      MenuItem("ให้คะแนนแอพ", Icons.star_outline),
+      MenuItem("กดไลค์ให้เราที่ Facebook", Icons.thumb_up_outlined),
+      MenuItem("โซลูชันสำหรับการโดยสารไปทำงาน", Icons.work_outline),
+      MenuItem("งานที่โบลต์", Icons.favorite_outline),
+      MenuItem("กฎหมาย", Icons.rule_outlined),
+      MenuItem("ความเป็นส่วนตัว", Icons.description_outlined),
+    ];
+
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: Container(
-        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: viewModel.onClose, icon: const Icon(Icons.arrow_back)),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "เกี่ยวกับ",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 23,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                Text(
+                  "Version CI.0001.0 (000000)",
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 15),
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: ListView.builder(
+              itemCount: menuLists.length,
+              itemBuilder: (context, index) {
+                MenuItem menu = menuLists[index];
+                return ListTile(
+                  title: Text(menu.title),
+                  leading: Icon(menu.icon),
+                  trailing: const Icon(
+                    Icons.arrow_forward_ios,
+                    size: 12,
+                    color: Colors.black38,
+                  ),
+                  shape: const Border(
+                    bottom: BorderSide(color: Colors.black12, width: 1.0),
+                  ),
+                );
+              },
+            ),
+          ))
+        ],
       ),
     );
   }
 
+/* 
+
+
+ */
   @override
   AboutViewModel viewModelBuilder(
     BuildContext context,
--- lib/ui/views/about/about_viewmodel.dart
+++ lib/ui/views/about/about_viewmodel.dart
@@ -1,3 +1,20 @@
+import 'package:boltclone_stacked/app/app.router.dart';
+import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:boltclone_stacked/app/app.locator.dart';
 
-class AboutViewModel extends BaseViewModel {}
+class MenuItem {
+  IconData icon;
+  String title;
+
+  MenuItem(this.title, this.icon);
+}
+
+class AboutViewModel extends BaseViewModel {
+  final _navigationService = locator<NavigationService>();
+
+  void onClose() {
+    _navigationService.navigateToHomeView();
+  }
+}
