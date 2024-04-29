41ac1a59a908764c9e799fae46d3e6e68ee5fd6e
iwind <134185860+iwindd@users.noreply.github.com>
2024-04-29 14:14:01 +0700

    top content

--- lib/ui/widgets/HomeDrawer.dart
+++ lib/ui/widgets/HomeDrawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeDrawer extends StatefulWidget {
   const HomeDrawer({super.key});
@@ -11,35 +12,64 @@ class _HomeDrawerState extends State<HomeDrawer> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      // Add a ListView to the drawer. This ensures the user can scroll
-      // through the options in the drawer if there isn't enough vertical
-      // space to fit everything.
-      child: ListView(
-        // Important: Remove any padding from the ListView.
-        padding: EdgeInsets.zero,
-        children: [
-          const DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.white,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(0), bottomRight: Radius.circular(0))),
+        backgroundColor: Colors.grey,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            Container(
+              height: 100,
+              alignment: Alignment.bottomCenter,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                ),
+              ),
+              child: const Align(
+                alignment: Alignment.center,
+                child: ListTile(
+                  title: Text(
+                    "Title",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w800),
+                  ),
+                  subtitle: Text(
+                    "บัญชีของฉัน",
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 13,
+                    ),
+                  ),
+                  leading: CircleAvatar(
+                    radius: 40,
+                    child: Icon(
+                      Icons.person,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
             ),
-            child: Text('Drawer Header'),
-          ),
-          ListTile(
-            title: const Text('Item 1'),
-            onTap: () {
-              // Update the state of the app.
-              // ...
-            },
-          ),
-          ListTile(
-            title: const Text('Item 2'),
-            onTap: () {
-              // Update the state of the app.
-              // ...
-            },
-          ),
-        ],
-      ),
-    );
+            const SizedBox(
+              height: 8,
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height - 108,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
