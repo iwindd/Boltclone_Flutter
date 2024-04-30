8543fa9e3ad8fb1fe71a8b76b1c169a9df50430a
iwind <134185860+iwindd@users.noreply.github.com>
2024-04-30 16:31:12 +0700

    history view

--- lib/ui/views/history/history_view.dart
+++ lib/ui/views/history/history_view.dart
@@ -4,7 +4,7 @@ import 'package:stacked/stacked.dart';
 import 'history_viewmodel.dart';
 
 class HistoryView extends StackedView<HistoryViewModel> {
-  const HistoryView({Key? key}) : super(key: key);
+  const HistoryView({super.key});
 
   @override
   Widget builder(
@@ -12,10 +12,47 @@ class HistoryView extends StackedView<HistoryViewModel> {
     HistoryViewModel viewModel,
     Widget? child,
   ) {
+    final theme = Theme.of(context);
+
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: Container(
-        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: viewModel.onClose, icon: const Icon(Icons.arrow_back)),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            decoration: BoxDecoration(
+              color: theme.colorScheme.surface,
+              borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(15),
+                  bottomRight: Radius.circular(15)),
+            ),
+            width: MediaQuery.of(context).size.width,
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "การโดยสารของฉัน",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 23,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              alignment: Alignment.center,
+              child: const Text("คุณยังไม่ได้ใช้บริการโดยสารใดๆ", style: TextStyle(color: Colors.grey),),
+            ),
+          )
+        ],
       ),
     );
   }
