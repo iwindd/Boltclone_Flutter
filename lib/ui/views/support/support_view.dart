2bfce5faafda3ef6d114a46c0fbd0f2d817d5de9
iwind <134185860+iwindd@users.noreply.github.com>
2024-04-30 17:22:38 +0700

    support page

--- lib/ui/views/support/support_view.dart
+++ lib/ui/views/support/support_view.dart
@@ -4,7 +4,7 @@ import 'package:stacked/stacked.dart';
 import 'support_viewmodel.dart';
 
 class SupportView extends StackedView<SupportViewModel> {
-  const SupportView({Key? key}) : super(key: key);
+  const SupportView({super.key});
 
   @override
   Widget builder(
@@ -12,10 +12,120 @@ class SupportView extends StackedView<SupportViewModel> {
     SupportViewModel viewModel,
     Widget? child,
   ) {
+    final theme = Theme.of(context);
+    List<MenuItem> menuLists = [
+      MenuItem("เกี่ยวกับ Bolt"),
+      MenuItem("แอป และคุณสมบัติ"),
+      MenuItem("บัญชี และ ข้อมูล"),
+      MenuItem("ราคาและการชำระค่าโดยสาร"),
+      MenuItem("การใช้งาน Bolt"),
+    ];
+
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Container(
-        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+      backgroundColor: theme.colorScheme.background,
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: viewModel.onClose, icon: const Icon(Icons.arrow_back)),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: const Text(
+              "เราช่วยได้อย่างไร",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 23,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Container(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "เคสการสนับสนุน",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                ListTile(
+                  contentPadding: EdgeInsets.all(0),
+                  leading: CircleAvatar(
+                    backgroundColor: Color.fromARGB(255, 244, 244, 244),
+                    child: Icon(Icons.chat_outlined),
+                  ),
+                  title: Text(
+                    "กล่องข้อความ",
+                    style: TextStyle(fontSize: 14),
+                  ),
+                  subtitle: Text(
+                    "ดูแชทที่เปิดอยู่",
+                    style: TextStyle(fontSize: 10, color: Colors.grey),
+                  ),
+                  trailing: Icon(
+                    Icons.arrow_forward_ios,
+                    size: 14,
+                    color: Colors.black38,
+                  ),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(height: 10),
+          Container(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "รับความช่วยเหลือในเรื่องอื่นๆ",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w800,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: ListView.builder(
+                itemCount: menuLists.length,
+                itemBuilder: (context, index) {
+                  MenuItem menu = menuLists[index];
+
+                  return ListTile(
+                    title: Text(menu.title),
+                    contentPadding: const EdgeInsets.all(0),
+                    trailing: const Icon(
+                      Icons.arrow_forward_ios,
+                      size: 12,
+                      color: Colors.black38,
+                    ),
+                    shape: index < menuLists.length - 1
+                        ? const Border(
+                            bottom:
+                                BorderSide(color: Colors.black12, width: 1.0),
+                          )
+                        : null,
+                  );
+                },
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/ui/views/support/support_viewmodel.dart
+++ lib/ui/views/support/support_viewmodel.dart
@@ -1,3 +1,17 @@
+import 'package:boltclone_stacked/app/app.locator.dart';
+import 'package:boltclone_stacked/app/app.router.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
-class SupportViewModel extends BaseViewModel {}
+class MenuItem {
+  String title;
+
+  MenuItem(this.title);
+}
+class SupportViewModel extends BaseViewModel {
+  final _navigationService = locator<NavigationService>();
+
+  void onClose() {
+    _navigationService.navigateToHomeView();
+  }
+}
